5b7f5339868e3bd1c21f89f4dc99655c2c77b5db
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-04 21:21:08 +0100

    All pages added

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+
+// const HomePage = () => {
+
+//     //const / query / handler etc
+
+//     return(
+//         <div className="homepage">
+//             //HTML/CSS/inhoud
+//         </div>
+//     )
+// }
+
+// export default HomePage;
+ 
