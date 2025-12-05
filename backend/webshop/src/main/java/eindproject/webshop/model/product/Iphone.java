bfd0955c22abcacf9dea25b36e002ae8f93bd225
Josh <j.wielings@gmail.com>
2025-12-05 01:32:29 +0100

    Updated product models for proper inheritance with hibernate, added find by category method to product repo

--- backend/webshop/src/main/java/eindproject/webshop/model/product/Iphone.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/product/Iphone.java
@@ -1,7 +1,9 @@
 package eindproject.webshop.model.product;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("iphone")
 public class Iphone extends Product {
 }
