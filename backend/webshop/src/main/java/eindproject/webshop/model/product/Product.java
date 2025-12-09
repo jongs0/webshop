158bb4c9ebd3592795cd91f6e8637d1055c3d978
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-09 14:57:45 +0100

    Product Entity filled with values

--- backend/webshop/src/main/java/eindproject/webshop/model/product/Product.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/product/Product.java
@@ -1,15 +1,96 @@
 package eindproject.webshop.model.product;
 
 import eindproject.webshop.model.enums.product.Category;
-import jakarta.persistence.DiscriminatorColumn;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import eindproject.webshop.model.enums.product.State;
+import jakarta.persistence.*;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "product_category")
 public class Product {
 
-    Category category;
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String description;
+    private String model;
+    private Double price;
+    private Integer stock;
+    private boolean productEnabled;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public boolean isProductEnabled() {
+        return productEnabled;
+    }
+
+    public void setProductEnabled(boolean productEnabled) {
+        this.productEnabled = productEnabled;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
