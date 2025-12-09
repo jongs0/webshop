a181a91e87788211ca77d1ca1d98a3002c72f216
Josh <j.wielings@gmail.com>
2025-12-09 14:18:08 +0100

    added iphone enums

--- backend/webshop/src/main/java/eindproject/webshop/model/enums/iphone/Generation.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/enums/iphone/Generation.java
@@ -1,4 +1,16 @@
 package eindproject.webshop.model.enums.iphone;
 
 public enum Generation {
-}
+
+        GEN_13("13"),
+        GEN_14("14"),
+        GEN_15("15"),
+        GEN_16("16"),
+        GEN_17("17");
+
+        private final String label;
+
+        Generation(String label) { this.label = label; }
+
+        public String getLabel() { return label; }
+    }
