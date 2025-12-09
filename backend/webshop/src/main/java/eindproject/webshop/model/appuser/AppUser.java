d881954d78cd16652aa120ed4e57c6837c44ee03
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-09 15:51:08 +0100

    User Entity values added

--- backend/webshop/src/main/java/eindproject/webshop/model/appuser/AppUser.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/appuser/AppUser.java
@@ -1,7 +1,39 @@
 package eindproject.webshop.model.appuser;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import eindproject.webshop.model.Role;
+import eindproject.webshop.model.order.Order;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "appUser_roles", joinColumns = @JoinColumn(name = "appUser_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private List<Role> roles = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Adress adress;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
+
 }
