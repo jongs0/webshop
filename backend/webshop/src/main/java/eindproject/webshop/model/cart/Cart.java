31188bd16c911732c3dde925b634990fe23951b3
Josh <j.wielings@gmail.com>
2025-12-10 12:09:25 +0100

    Added Cart entity

--- backend/webshop/src/main/java/eindproject/webshop/model/cart/Cart.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/cart/Cart.java
@@ -1,7 +1,42 @@
 package eindproject.webshop.model.cart;
 
-import jakarta.persistence.Entity;
+import eindproject.webshop.model.appuser.AppUser;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Cart {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    private AppUser appUser;
+
+    @OneToMany(mappedBy = "cart")
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
 }
+
