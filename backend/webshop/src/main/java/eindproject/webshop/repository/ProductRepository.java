d4e447814765dfcbd84501b54990867e6ab92596
Sietse de Jong <sietse.dejong68@gmail.com>
2025-12-10 15:46:30 +0100

    Add CartDTO and CartProductDTO

--- backend/webshop/src/main/java/eindproject/webshop/repository/ProductRepository.java
+++ backend/webshop/src/main/java/eindproject/webshop/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package eindproject.webshop.repository;
 
 import eindproject.webshop.model.enums.product.Category;
 import eindproject.webshop.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(Category category);
 }
--- backend/webshop/src/main/java/eindproject/webshop/service/ProductService.java
+++ backend/webshop/src/main/java/eindproject/webshop/service/ProductService.java
@@ -1,7 +1,22 @@
 package eindproject.webshop.service;
 
+import eindproject.webshop.model.product.Product;
+import eindproject.webshop.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product findEntityById(Long id) {
+        return productRepository.findById(id)
+                 .orElseThrow(() -> new EntityNotFoundException("Product not found with ID: " + id));
+    }
 }
