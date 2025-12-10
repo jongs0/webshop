b91b25ac47772303ffd52853c2f39802629b57e3
Sietse de Jong <sietse.dejong68@gmail.com>
2025-12-10 11:29:33 +0100

    Add CartItem model

--- backend/webshop/src/main/java/eindproject/webshop/model/cart/CartItem.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/cart/CartItem.java
@@ -1,7 +1,17 @@
 package eindproject.webshop.model.cart;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class CartItem {
+
+    @GeneratedValue
+    @Id
+    Long id;
+    Long productId;
+    Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name="cart_id")
+    Cart cart;
 }
