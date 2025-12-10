53da7449575069345af9d05b2a843474997a1818
Josh <j.wielings@gmail.com>
2025-12-10 13:47:38 +0100

    created ipad DTOs

--- backend/webshop/src/main/java/eindproject/webshop/dto/product/ipad/IpadCreateDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/product/ipad/IpadCreateDTO.java
@@ -1,4 +1,61 @@
 package eindproject.webshop.dto.product.ipad;
 
-public record IpadCreateDTO() {
+import eindproject.webshop.model.enums.ipad.*;
+import eindproject.webshop.model.enums.product.State;
+import eindproject.webshop.model.product.Ipad;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record IpadCreateDTO(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        String description,
+
+        @NotNull
+        Double price,
+
+        @NotNull
+        Integer stock,
+
+        @NotNull
+        State state,
+
+        @NotBlank
+        String model,
+
+        @NotNull
+        Generation generation,
+
+        @NotNull
+        Storage storage,
+
+        @NotNull
+        Connectivity connectivity,
+
+        @NotNull
+        Color color
+
+) {
+
+    public Ipad toEntity() {
+
+        Ipad ipad = new Ipad();
+
+        ipad.setName(this.name);
+        ipad.setDescription(this.description);
+        ipad.setPrice(this.price);
+        ipad.setStock(this.stock);
+        ipad.setState(this.state);
+        ipad.setModel(this.model);
+
+        ipad.setGeneration(this.generation);
+        ipad.setStorage(this.storage);
+        ipad.setConnectivity(this.connectivity);
+        ipad.setColor(this.color);
+
+        return ipad;
+    }
 }
--- backend/webshop/src/main/java/eindproject/webshop/dto/product/ipad/IpadDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/product/ipad/IpadDTO.java
@@ -1,4 +1,42 @@
 package eindproject.webshop.dto.product.ipad;
 
-public record IpadDTO() {
+import eindproject.webshop.model.enums.ipad.Color;
+import eindproject.webshop.model.enums.ipad.Connectivity;
+import eindproject.webshop.model.enums.ipad.Generation;
+import eindproject.webshop.model.enums.ipad.Storage;
+import eindproject.webshop.model.enums.product.State;
+import eindproject.webshop.model.product.Ipad;
+
+public record IpadDTO(
+
+        String name,
+        String description,
+        Double price,
+        Integer stock,
+        State state,
+        String model,
+
+        Generation generation,
+        Storage storage,
+        Color color,
+        Connectivity connectivity
+
+) {
+    public static IpadDTO fromEntity(Ipad ipad) {
+        return new IpadDTO(
+
+        ipad.getName(),
+        ipad.getDescription(),
+        ipad.getPrice(),
+        ipad.getStock(),
+        ipad.getState(),
+        ipad.getModel(),
+
+        ipad.getGeneration(),
+        ipad.getStorage(),
+        ipad.getColor(),
+        ipad.getConnectivity()
+
+        );
+    }
 }
