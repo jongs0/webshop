3bcec6aecde6e82332931c36a2bff7b064ec04df
Patrick Boot <raventus@hotmail.nl>
2025-12-10 10:50:47 +0100

    AppUserDTO en AppUserCreateDTO

--- backend/webshop/src/main/java/eindproject/webshop/dto/appuser/AppUserCreateDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/appuser/AppUserCreateDTO.java
@@ -1,4 +1,24 @@
 package eindproject.webshop.dto.appuser;
 
-public record AppUserCreateDTO() {
-}
+import eindproject.webshop.model.appuser.AppUser;
+import jakarta.validation.constraints.NotBlank;
+
+public record AppUserCreateDTO(
+        @NotBlank(message = "Fields cannot be empty")
+        String email,
+        @NotBlank(message = "Fields cannot be empty")
+        String password,
+        @NotBlank(message = "Fields cannot be empty")
+        String firstName,
+        @NotBlank(message = "Fields cannot be empty")
+        String lastName
+        ) {
+    public AppUser toEntity() {
+        AppUser user = new AppUser();
+        user.setEmail(this.email);
+        user.setPassword(this.password);
+        // vervang setPassword met een hash later als we security doen
+        user.setFirstName(this.firstName);
+        user.setLastName(this.lastName);
+    }
+    }
--- backend/webshop/src/main/java/eindproject/webshop/dto/appuser/AppUserDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/appuser/AppUserDTO.java
@@ -1,4 +1,28 @@
 package eindproject.webshop.dto.appuser;
 
-public record AppUserDTO() {
+import eindproject.webshop.dto.adress.AdressDTO;
+import eindproject.webshop.dto.order.OrderDTO;
+import eindproject.webshop.model.appuser.AppUser;
+import java.util.List;
+
+public record AppUserDTO(
+        Long id,
+        String email,
+        String firstName,
+        String lastName,
+        AdressDTO address,
+        List<OrderDTO> orders
+) {
+    // Double-check getters when AppUser model is implemented
+    // Make sure OrderDTO has fromEntity
+    public static AppUserDTO fromEntity(AppUser appUser) {
+        return new AppUserDTO(
+                appUser.getId(),
+                appUser.getEmail(),
+                appUser.getFirstName(),
+                appUser.getLastName(),
+                appUser.getAddress(),
+                appUser.getOrders().stream().map(OrderDTO::fromEntity).toList()
+        );
+    }
 }
