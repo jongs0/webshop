505aed3df534459b69321ab9b70c169751606657
Josh <j.wielings@gmail.com>
2025-12-10 13:22:05 +0100

    Added iPhone DTO's

--- backend/webshop/src/main/java/eindproject/webshop/dto/product/iphone/IphoneSummaryDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/product/iphone/IphoneSummaryDTO.java
@@ -1,4 +1,27 @@
 package eindproject.webshop.dto.product.iphone;
 
-public record IphoneSummaryDTO() {
+import eindproject.webshop.model.enums.iphone.Generation;
+import eindproject.webshop.model.product.Iphone;
+
+public record IphoneSummaryDTO(
+
+        Long id,
+        String name,
+        Double price,
+        String model,
+        Generation generation,
+        Integer stock
+
+) {
+
+    public static IphoneSummaryDTO fromEntity(Iphone iphone) {
+        return new IphoneSummaryDTO(
+                iphone.getId(),
+                iphone.getName(),
+                iphone.getPrice(),
+                iphone.getModel(),
+                iphone.getGeneration(),
+                iphone.getStock()
+        );
+    }
 }
