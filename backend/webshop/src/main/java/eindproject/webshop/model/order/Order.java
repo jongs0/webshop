3d7d4f2dfd3b64b5d855623edb99bb001125ed1d
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-10 14:38:47 +0100

    Order Entity added

--- backend/webshop/src/main/java/eindproject/webshop/model/order/Order.java
+++ backend/webshop/src/main/java/eindproject/webshop/model/order/Order.java
@@ -1,7 +1,89 @@
 package eindproject.webshop.model.order;
 
-import jakarta.persistence.Entity;
+import eindproject.webshop.model.appuser.Adress;
+import eindproject.webshop.model.appuser.AppUser;
+import eindproject.webshop.model.enums.PaymentMethod;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+
+    private Double totalSum;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser appUser;
+
+    @Enumerated(EnumType.STRING)
+    private List<PaymentMethod> paymentMethod;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @ManyToOne
+    private Adress address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Double getTotalSum() {
+        return totalSum;
+    }
+
+    public void setTotalSum(Double totalSum) {
+        this.totalSum = totalSum;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public List<PaymentMethod> getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(List<PaymentMethod> paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+    }
+
+    public void removeOrderItem(OrderItem orderItem){
+        orderItems.remove(orderItem);
+    }
+
+//    public void setOrderItems(List<OrderItem> orderItems) {
+//        this.orderItems = orderItems;
+//    }
+
+    public Adress getAddress() {
+        return address;
+    }
+
+    public void setAddress(Adress address) {
+        this.address = address;
+    }
 }
