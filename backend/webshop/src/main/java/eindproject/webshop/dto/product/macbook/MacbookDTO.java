d9abd9c85abec01939260f949e75d2b73f8dc5ac
Patrick Boot <raventus@hotmail.nl>
2025-12-10 14:24:17 +0100

    MacBookDTO

--- backend/webshop/src/main/java/eindproject/webshop/dto/product/macbook/MacbookDTO.java
+++ backend/webshop/src/main/java/eindproject/webshop/dto/product/macbook/MacbookDTO.java
@@ -1,4 +1,50 @@
 package eindproject.webshop.dto.product.macbook;
 
-public record MacbookDTO() {
+import eindproject.webshop.model.enums.macbook.*;
+import eindproject.webshop.model.enums.product.*;
+import eindproject.webshop.model.product.Macbook;
+import jakarta.validation.constraints.*;
+
+public record MacbookDTO(
+        @NotNull
+        Long id,
+        @NotBlank
+        String name,
+        @NotBlank
+        String description,
+        @NotNull
+        Double price,
+        @NotNull
+        Integer stock,
+        @NotNull
+        State state,
+        @NotBlank
+        String model,
+        @NotNull
+        Integer releaseYear,
+        @NotNull
+        ChipType chipType,
+        @NotNull
+        RamSize ram,
+        @NotNull
+        Storage storage,
+        @NotNull
+        Color color)
+{
+    public MacbookDTO fromEntity(Macbook macbook) {
+        return new MacbookDTO(
+                macbook.getId(),
+                macbook.getName(),
+                macbook.getDescription(),
+                macbook.getPrice(),
+                macbook.getStock(),
+                macbook.getState(),
+                macbook.getModel(),
+                macbook.getReleaseYear(),
+                macbook.getChipType(),
+                macbook.getRamSize(),
+                macbook.getStorage(),
+                macbook.getColor()
+        );
+    }
 }
