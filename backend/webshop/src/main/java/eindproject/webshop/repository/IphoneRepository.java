4d9b4a6031e3f5f2f720d4b293cdd3e0da0545d7
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-11 19:15:33 +0100

    Added Iphone Repository

---
+++ backend/webshop/src/main/java/eindproject/webshop/repository/IphoneRepository.java
@@ -0,0 +1,8 @@
+package eindproject.webshop.repository;
+
+import eindproject.webshop.model.product.Iphone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IphoneRepository extends JpaRepository<Iphone, Long>{
+
+}
