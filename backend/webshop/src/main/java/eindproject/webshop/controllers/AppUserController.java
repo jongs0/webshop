270137f9b6bc23a24934e91de38e9cb7bda326a5
Patrick Boot <raventus@hotmail.nl>
2025-12-11 12:57:47 +0100

    Initial setup

--- backend/webshop/src/main/java/eindproject/webshop/controllers/AppUserController.java
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/AppUserController.java
@@ -1,5 +1,7 @@
 package eindproject.webshop.controllers;
 
+import eindproject.webshop.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("examplepath")
 public class AppUserController {
+    private final AppUserService appUserService;
+
+    @Autowired
+    public AppUserController(AppUserService appUserService) { this.appUserService = appUserService; }
+
+    // Incoming POST for new users - createUser method
+    // Incoming GET for single user (for profile page) - getUser method
+    // Incoming GET for all users (admin-only) - getAllUsers method
+    // Incoming PUT for data update - updateUser method
+    // optional incoming DELETE for single user (admin-only) - deleteUser method
+
+
 }
--- backend/webshop/src/main/java/eindproject/webshop/service/AppUserService.java
+++ backend/webshop/src/main/java/eindproject/webshop/service/AppUserService.java
@@ -1,7 +1,21 @@
 package eindproject.webshop.service;
 
+import eindproject.webshop.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppUserService {
+    final private AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserService(AppUserRepository appUserRepository) {this.appUserRepository = appUserRepository;}
+
+    // Incoming POST for new users - createUser method
+    // Incoming GET for single user (for profile page) - getUser method
+    // Incoming GET for all users (admin-only) - getAllUsers method
+    // Incoming PUT for data update - updateUser method
+    // optional incoming DELETE for single user (admin-only) - deleteUser method
+
+
 }
