8f4103ec5e7069cb79c6c153dfa64dea7d5c50ed
Josh <j.wielings@gmail.com>
2025-12-12 00:22:13 +0100

    Added controllers for order and cart, added checkout method to order controller, deleted checkout controller

--- backend/webshop/src/main/java/eindproject/webshop/controllers/CartController.java
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/CartController.java
@@ -4,8 +4,47 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import eindproject.webshop.dto.cart.CartDTO;
+import eindproject.webshop.service.CartService;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @CrossOrigin("*")
-@RequestMapping("examplepath")
+@RequestMapping("/cart")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/{appUserId}")
+    public CartDTO getCart(@PathVariable Long appUserId) {
+        return cartService.getCart(appUserId);
+    }
+
+    @PostMapping("/{appUserId}/add/{productId}")
+    public CartDTO addToCart(
+            @PathVariable Long appUserId,
+            @PathVariable Long productId,
+            @RequestParam int quantity
+    ) {
+        return cartService.addToCart(appUserId, productId, quantity);
+    }
+
+
+    @PutMapping("/{appUserId}/update/{productId}")
+    public CartDTO updateItem(
+            @PathVariable Long appUserId,
+            @PathVariable Long productId,
+            @RequestParam int quantity
+    ) {
+        return cartService.updateItem(appUserId, productId, quantity);
+    }
+
+    @DeleteMapping("/{appUserId}/clear")
+    public CartDTO clearCart(@PathVariable Long appUserId) {
+        return cartService.clearCart(appUserId);
+    }
 }
--- backend/webshop/src/main/java/eindproject/webshop/controllers/OrderController.java
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/OrderController.java
@@ -3,9 +3,52 @@ package eindproject.webshop.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import eindproject.webshop.dto.order.OrderCreateDTO;
+import eindproject.webshop.dto.order.OrderDTO;
+import eindproject.webshop.model.enums.PaymentMethod;
+import eindproject.webshop.service.CheckOutService;
+import eindproject.webshop.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("examplepath")
+@RequestMapping("/order")
 public class OrderController {
+
+    private final OrderService orderService;
+    private final CheckOutService checkOutService;
+
+    public OrderController(OrderService orderService,
+                           CheckOutService checkOutService) {
+        this.orderService = orderService;
+        this.checkOutService = checkOutService;
+    }
+
+    @PostMapping("/{appUserId}/checkout")
+    public OrderDTO checkout(
+            @PathVariable Long appUserId,
+            @RequestParam PaymentMethod paymentMethod
+    ) {
+        return checkOutService.checkout(appUserId, paymentMethod);
+    }
+
+
+    @GetMapping("/{orderId}")
+    public OrderDTO getOrder(@PathVariable Long orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+
+    @GetMapping("/user/{appUserId}")
+    public List<OrderDTO> getOrdersForUser(@PathVariable Long appUserId) {
+        return orderService.getOrdersForUser(appUserId);
+    }
+
+
+    @GetMapping // deze moeten we met sec. config. permission based maken
+    public List<OrderDTO> getAllOrders() {
+        return orderService.getAllOrders();
+    }
 }
