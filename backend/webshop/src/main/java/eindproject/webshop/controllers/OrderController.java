94193c53734f1e4e19682f66c619867d27986fc2
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-11 15:21:22 +0100

    Order create Begin in controller and service but stopped due to missing files yet to be made.

--- backend/webshop/src/main/java/eindproject/webshop/controllers/OrderController.java
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/OrderController.java
@@ -1,11 +1,37 @@
 package eindproject.webshop.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import eindproject.webshop.dto.order.OrderDTO;
+import eindproject.webshop.model.order.Order;
+import eindproject.webshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("examplepath")
+@RequestMapping("orders")
 public class OrderController {
+
+    //1. import service hier voor order en andere zoals cart
+//    final private OrderService orderService;
+//
+//    @Autowired
+//    public OrderController(OrderService orderService) {
+//        this.orderService = orderService;
+//    }
+
+    //2. waar komt de create order methode? (kan ook namelijk eerder in de flow dan hier)
+
+    //3. Postmapping voor create
+//    @PostMapping("/create/")
+//    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO dto) {
+//        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.createOrder(dto));
+//    }
+
+    //Notities van Mike:
+//        @PostMapping("/create/{userId}")
+//        public OrderDTO create(RequestBody OrderDTO order) {
+//        }??
+
 }
--- backend/webshop/src/main/java/eindproject/webshop/service/OrderService.java
+++ backend/webshop/src/main/java/eindproject/webshop/service/OrderService.java
@@ -1,7 +1,33 @@
 package eindproject.webshop.service;
 
+import eindproject.webshop.model.order.Order;
+import eindproject.webshop.repository.OrderRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    //1. inladen van repo
+//    final private OrderRepository orderRepository;
+
+//    @Autowired
+//    public OrderService(OrderRepository orderRepository) {
+//        this.orderRepository = orderRepository;
+//    }
+
+    //1. Create order methode
+//    public Order createOrder(){
+//
+//    }
+
+    // maak een methode die de cart/winkelwagen producten overzet naar de tabellen van order
+//    public Order createOrderFromCart(){
+//
+//    }
+
+
+    //clear de cart met een methode
+
 }
