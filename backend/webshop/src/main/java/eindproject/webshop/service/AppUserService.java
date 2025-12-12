2eec43d5a5c704f6057c7081db683e8650662db3
Patrick Boot <raventus@hotmail.nl>
2025-12-12 13:23:14 +0100

    added Address to AppUserUpdateDTO part 2

--- backend/webshop/src/main/java/eindproject/webshop/service/AppUserService.java
+++ backend/webshop/src/main/java/eindproject/webshop/service/AppUserService.java
@@ -81,6 +81,9 @@ public class AppUserService {
             if (!Objects.equals(appUseruser.getLastName(), updateDTO.lastName())) {
                 appUseruser.setLastName(updateDTO.lastName());
             }
+            if (!Objects.equals(appUseruser.getAdress(), updateDTO.address()) {
+                appUseruser.setAdress(updateDTO.address());
+        }
         }
         appUserRepository.save(appUseruser);
         return AppUserSummaryDTO.fromEntity(appUseruser);
