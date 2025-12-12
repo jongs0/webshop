2a9db4f384bcfb3bb801039f3dc6387f29dd5156
Josh <j.wielings@gmail.com>
2025-12-12 13:14:11 +0100

    Added macbook service + controller, updated dto's

---
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/MacbookController.java
@@ -0,0 +1,51 @@
+package eindproject.webshop.controllers;
+
+
+import eindproject.webshop.dto.product.macbook.MacbookCreateDTO;
+import eindproject.webshop.dto.product.macbook.MacbookDTO;
+import eindproject.webshop.dto.product.macbook.MacbookUpdateDTO;
+import eindproject.webshop.service.MacbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/macbook")
+public class MacbookController {
+
+    private final MacbookService macbookService;
+
+    @Autowired
+    public MacbookController(MacbookService macbookService) {
+        this.macbookService = macbookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MacbookDTO> create(@RequestBody MacbookCreateDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(macbookService.createMacbook(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MacbookDTO> getMacbookById(@PathVariable Long id) {
+        return ResponseEntity.ok(macbookService.getMacbookById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MacbookDTO>> getAllMacbooks() {
+        return ResponseEntity.ok(macbookService.getAllMacbooks());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MacbookDTO> update(@PathVariable Long id, @RequestBody MacbookUpdateDTO dto
+    ) {
+        return ResponseEntity.ok(macbookService.updateMacbook(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {macbookService.deleteMacbook(id);
+    }
+}
