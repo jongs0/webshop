39991f5c2bc9fad7ecf455b1a4402379b7c6ac57
Josh <j.wielings@gmail.com>
2025-12-12 13:20:48 +0100

    Added a dto to get all products for admin panel, updated product service and controller to facilitate

--- backend/webshop/src/main/java/eindproject/webshop/controllers/ProductController.java
+++ backend/webshop/src/main/java/eindproject/webshop/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package eindproject.webshop.controllers;
 
+import eindproject.webshop.dto.product.ProductAdminSummaryDTO;
 import eindproject.webshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -18,5 +22,8 @@ public class ProductController {
         this.productService = productService;
     }
 
-
+    @GetMapping("/admin")
+    public List<ProductAdminSummaryDTO> getAllProductsForAdmin() {
+        return productService.getAllProductsForAdmin();
+    }
 }
