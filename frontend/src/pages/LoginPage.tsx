9284ddd82e2d3326055ec006eebee7b861773af5
Patrick Boot <raventus@hotmail.nl>
2025-12-17 11:11:04 +0100

    Paused due to required dependencies

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
@@ -9,6 +10,9 @@ interface LoginData {
 const LoginPage = () => {
     const [formData, setFormData] = useState<LoginData>({email:"",password:""});
 
+    // useMutation for login
+
+    // handleSubmit
 
     const handleChangeBootstrap = (event: any) => {
         let fieldName = event.target.name;
@@ -18,6 +22,7 @@ const LoginPage = () => {
     }
 
     return (
+        // header goes here
         <Form>
             <h1>LOGIN</h1>
             <div/>
@@ -30,10 +35,12 @@ const LoginPage = () => {
                 <Form.Control name="password" type="password" placeholder="Password" onChange={handleChangeBootstrap}/>
             </Form.Group>
             <p/>
+            <Button variant="primary" type="submit">Register
+            </Button>
             <Button variant="primary" type="submit">Submit
             </Button>
         </Form>
-
+        // footer goes here
     )
 }
 
