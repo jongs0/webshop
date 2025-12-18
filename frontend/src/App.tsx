7eaffe3ed673cc6739c25ff61ddd7d2133c8bebb
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-18 17:17:22 +0100

    Imports updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import './styles/generalStyling.css';
-import './styles/buttonStyling.css';
+import './styles/general/generalStyling.css';
+import './styles/general/buttonStyling.css';
 import './styles/pages/homepageStyling.css';
-import './styles/imageStyling.css';
+import './styles/general/imageStyling.css';
 
 
 
