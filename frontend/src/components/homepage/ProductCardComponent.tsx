99e4bb213f612ec06cf64b21918d367e8e3ce491
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-12-19 11:52:16 +0100

    Component strcuture for product cards added

--- frontend/src/components/homepage/ProductCardComponent.tsx
+++ frontend/src/components/homepage/ProductCardComponent.tsx
@@ -1,30 +1,60 @@
-// import { CardImg } from 'react-bootstrap';
-// import Card from 'react-bootstrap/Card';
-// import Col from 'react-bootstrap/Col';
-// import Row from 'react-bootstrap/Row';
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../App";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import { getProductCategoryImage } from "./ProductImageCategoryHelper";
+import { Link } from "react-router";
 
-// const ProductCardComponent = () => {
+interface ProductCardProps {
+  queryKey: string[];
+  endpoint: string;
+  title?: string;
+}
 
-//     return (
-//         <div>
-//             <div  className="container category-list">
-//                 <p>Categroy List</p>
-//                 <Row xs={1} md={2} className="g-4">
-//                     {Array.from({ length: 4 }).map((_, idx) => (
-//                         <Col key={idx}>
-//                         <Card>
-//                             <Card.Img variant="top" src="holder.js/100px160" />
-//                             <Card.Body>
-//                             <CardImg></CardImg>    
-//                             <Card.Title>Catgory name</Card.Title>
-//                             </Card.Body>
-//                         </Card>
-//                         </Col>
-//                     ))}
-//                 </Row>
-//             </div>
-//         </div>
-//     )
-// }
+const ProductCardComponent = ({ queryKey, endpoint, title}: ProductCardProps) => {
+  const { data: products, isLoading, error } = useQuery<ProductDTO[]>({
+    queryKey,
+    queryFn: async () => {
+      const response = await fetch(`${API_URL}${endpoint}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch bestsellers");
+      }
+      return response.json();
+    },
+  });
 
-// export default ProductCardComponent;
+  // Loading / error guards
+  if (isLoading) return <p>Loading products...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {(error as Error).message}</p>;
+  if (!products || products.length === 0) return <p>Producten niet gevonden...</p>;
+
+  return (
+    <div className="container bestseller">
+      <Row className="g-4">
+        {products.map(product => (
+          <Col key={product.id} xs={12} sm={6} md={6} lg={3}>
+            <Card className="h-100">
+                <Link
+                    to={`/products/${product.id}`}
+                    className="text-decoration-none text-dark"
+                >
+              <Card.Img
+                variant="top"
+                src={getProductCategoryImage(product.category)}
+                alt={product.name}
+              />
+              <Card.Body>
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>€{product.price}</Card.Text>
+              </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default ProductCardComponent;
