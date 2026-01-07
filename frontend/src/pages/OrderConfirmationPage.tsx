154278053abba2e1df4e1cf5bd9b9e61f7075714
Patrick Boot <raventus@hotmail.nl>
2026-01-07 15:19:52 +0100

    Order Confirmation Page (basic functionality)

--- frontend/src/pages/OrderConfirmationPage.tsx
+++ frontend/src/pages/OrderConfirmationPage.tsx
@@ -1,27 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import type { OrderDTO } from "../types/models";
 import { API_URL } from "../App";
-import { useState } from "react";
 import { Link, useParams } from "react-router";
-import { Card, Col, Row } from "react-bootstrap";
-
-interface OrderQuery {
-    data: OrderDTO,
-    isLoading: boolean,
-    error: Error
-}
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { currentUser } from "../stores/UserStore";
 
 const OrderConfirmationPage = () => {
     const params = useParams()
+    const user = currentUser();
 
     const {
         data: fetchedOrder,
         isLoading: isLoading,
         error: fetchError
     } = useQuery<OrderDTO>({
-        meta: {headers: {"Authorization":}}
         queryKey: ["order"], queryFn: async () => {
-            const response = await fetch(`${API_URL}/order/${params.orderId}`);
+            const response = await fetch(`${API_URL}/order/${params.orderId}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': user.authHeader
+                }
+            });
             if (!response.ok) {
                 throw new Error("project error")
             }
@@ -30,7 +29,7 @@ const OrderConfirmationPage = () => {
     })
 
     if (isLoading) {
-        return <p>Loading...</p>
+        return <p>Loading order...</p>
     }
     if (fetchError) {
         return <p>The order could not be retrieved.</p>
@@ -39,31 +38,37 @@ const OrderConfirmationPage = () => {
 
     if (fetchedOrder !== undefined) {
         return (
-            <>
+            <Container className="justify-content-md-center">
                 <h1>Bedankt voor de bestelling!</h1>
-                <Row className="g-4">
-                    {fetchedOrder.orderItems.map(product => (
-                        <Col key={product.id} xs={12} sm={6} md={6} lg={3}>
-                            <Card className="h-100">
-                                <Link
-                                    to={`/products/${product.id}`}
-                                    className="text-decoration-none text-dark"
-                                >
-                                    {/* <Card.Img
+                <h6>Hij komt eraan, en veel plezier!</h6>
+                <Row/>
+                    <Col className="g-4,justify-content-centre">
+                                    {fetchedOrder.orderItems.map(product => (
+                            <Row className="justify-content-md-center" key={product.id} xs={12} sm={6} md={6} lg={3}>
+                                <Card className="h-100">
+                                    <Link
+                                        to={`/products/${product.id}`}
+                                        className="text-decoration-none text-dark"
+                                    >
+                                        {/* <Card.Img
                                         variant="top"
                                         src={getProductCategoryImage(product.category)}
                                         alt={product.name}
                                     /> */}
-                                    <Card.Body>
-                                        <Card.Title>{product.productName}</Card.Title>
-                                        <Card.Text>€{product.lineTotal}</Card.Text>
-                                    </Card.Body>
-                                </Link>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </>
+                                        <Card.Body>
+                                            <Card.Title>{product.productName}</Card.Title>
+                                            <Card.Text>{product.quantity}x = €{product.lineTotal}</Card.Text>
+                                            {/* <ListGroup className="list-group-flush">
+                                                <ListGroup.Item>Price: €{product.lineTotal}</ListGroup.Item>
+                                                <ListGroup.Item>Quantity: {product.quantity}</ListGroup.Item>
+                                            </ListGroup> */}
+                                        </Card.Body>
+                                    </Link>
+                                </Card>
+                            </Row>
+                        ))}
+                    </Col>
+            </Container>
         )
     }
 }
