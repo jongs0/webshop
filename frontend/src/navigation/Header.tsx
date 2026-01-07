dbd0fc8d63b0fac9aec85f58b61f3bb4ecbfb04c
Patrick Boot <raventus@hotmail.nl>
2026-01-07 13:35:54 +0100

    Testing purposes only, revert later

--- frontend/src/navigation/Header.tsx
+++ frontend/src/navigation/Header.tsx
@@ -1,4 +1,4 @@
-import { Image } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 import { Link } from "react-router";
 import "../styles/general/bannerStyling.css";
 import { currentUser } from "../stores/UserStore";
@@ -32,6 +32,7 @@ const Header = () => {
           </div>
 
           <div className="nav-right">
+            <Button onClick={() => navigate("cart/finalize/1")}>TEST</Button>
             <Link className="nav-link" to="/support">Support</Link>
             <Link className="nav-link" to="/cart">Cart</Link>
             <Link className="nav-link" to="/profile">Profile</Link>
